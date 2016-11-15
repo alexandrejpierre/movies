c1936b54e9b3037d883efaf3bc3337601a696a33
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2016-11-15 18:35:36 +0100

    Added a filter on Year with boundaries in the movies api

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -6,13 +6,21 @@ module Api
 		
 		def index
 			# Pre-filter to remove non-relevant data
-			@movies=Movie.where(Type:"movie").where.not(Poster:"N/A").where.not(Plot:"N/A")
+			@movies=Movie.where(Type:"movie").where.not(Poster:"N/A").where.not(Plot:"N/A").where.not(Title:"#DUPE#").where(freshest:"x")
 			params.each do |key,val|
 				if key != 'controller' and key != 'action' and key != 'movie'
-					@movies=@movies.where(key => val)
+					if key=='Year_min'
+						@movies=@movies.where("Year >= ?",val)
+					else if key=='Year_max'
+						@movies=@movies.where("Year <= ?",val)
+					else	
+						@movies=@movies.where(key => val)
+					end
+					end
 				end
 			end
 			respond_with @movies.first(100)
 		end
 	end
 end
+
