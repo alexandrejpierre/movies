d9626edc00fa9d0c02fb3b02359d815e4640d3d5
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2017-01-22 13:04:05 +0100

    Enhanced the movies API to performe LIKE filtering on Genre and Country

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -13,7 +13,10 @@ module Api
 				if key != 'controller' and key != 'action' and key != 'movie'
 					if key=='Year'
 						@movies=@movies.where(Year: val.slice(0,4)..val.slice(4,9))
-					else	
+			# 20170122: Added the elsif clause to fix the filtering on Genre and Country
+					elsif (key=='Genre' or key=='Country')
+						@movies=@movies.where("\"#{key}\" LIKE ?","%#{val}%")
+					else
 						@movies=@movies.where(key => val)
 					end
 				end
