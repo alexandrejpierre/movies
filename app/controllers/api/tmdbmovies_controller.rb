53eb33c057da9e63c5800e50d05f4e4d2a6039e5
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2017-02-03 22:49:31 +0100

    Changed the tmdb api to exclude the movies that the user has already seen

--- app/controllers/api/tmdbmovies_controller.rb
+++ app/controllers/api/tmdbmovies_controller.rb
@@ -6,11 +6,14 @@ module Api
 		
 		def index
 			# Initialisation from the tmdb table
+			# 20170203: retrieval of the user_id based on their email
+			u_id=User.where("email = ?",preferences_params[:email]).first.id
 			# 20170131: Removal of adult movies
-			@movies=Tmdbmovie.where(adult: false)
+			# 20170203: Removal of the films that the users has already seen
+			@movies=Tmdbmovie.joins("LEFT OUTER JOIN preferences ON preferences.tmdbmovie_id =tmdbmovies.id AND preferences.user_id=#{u_id}").where(adult: false)
 			# Filtering based on sent parameters
 			params.each do |key,val|
-				if key != 'controller' and key != 'action' and key != 'movie'
+				if key != 'controller' and key != 'action' and key != 'movie' and key !='email'
 					if key=='release_year'
 						@movies=@movies.where(release_year: val.slice(0,4)..val.slice(4,9))
 			# 20170122: Added the elsif clause to fix the filtering on Genre and Country
@@ -32,7 +35,7 @@ module Api
 			# Added a count of the number of movies
 			# 20170122: changed the number of movies returned from 100 to 500
 			# 20170122: identification of the returned movies thanks to the random sampling
-			# 20170131: sorted by popularity
+			# 20170131: sorted by popularity and limit to 100
 			render json: { count: @movies.count, data: @movies.order(popularity: :desc).first(100) }
 		end
 	end
