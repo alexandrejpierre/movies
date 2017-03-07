e63f15b7ee2cc6602d512006c38d5bf06f6234b9
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2017-03-07 11:55:45 +0100

    Used the relevant_columns scope in the favorites GET api

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -4,16 +4,17 @@ module Api
                 respond_to :json
 		
 		# 20170306: created the index action to retrieve the favorites of a user
+		# 20170307: used the relevant_columns scope to filter the output
                 def index
                         @u_id = User.find_by("email = ?",params[:email]).id
-			@favorites=Tmdbmovie.joins(:preferences).where("preferences.user_id = ?",@u_id).where("preferences.favorite = 'Y'")
+			@favorites=Tmdbmovie.relevant_columns.joins(:preferences).where("preferences.user_id = ?",@u_id).where("preferences.favorite = 'Y'")
                 	respond_with @favorites, location: nil
 		end
 
 		def update
 			@u_id=User.find_by("email = ?",update_favorites_params[:email]).id
         		@status=Preference.where("user_id = ?",@u_id).find_by("tmdbmovie_id = ?",update_favorites_params[:id]).update_attributes(favorite: update_favorites_params[:favorite])
-        		respond_with  @status, location: nil
+        		respond_with @status, location: nil
 		end
 
 		private
