a2cf4a603d40a5917533ed52072885036fe8e359
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2017-06-02 10:56:44 +0200

    Changed the relevant_movies scope

--- app/models/tmdbmovie.rb
+++ app/models/tmdbmovie.rb
@@ -11,7 +11,8 @@ class Tmdbmovie < ActiveRecord::Base
 	# 20170401: fixed the relevant_columns and added the fr columns
 	scope :relevant_columns, -> { select("tmdbmovies.id, genres, overview, poster_path, production_countries, runtime, spoken_languages, title, vote_average, vote_count, release_year, actors, director, trailer_url, popularity, adult, title_fr, overview_fr, trailer_url_fr, friends_likes")}
 	# 20170529: new scope to filter out weird movies
+	# 20170602: filtered out the blank fr overviews
 	def self.relevant_movies
-		where.not(overview: nil).where.not(vote_average: 0.0).where.not(poster_path: nil).where.not(production_countries: nil).where.not(actors: nil)
+		where.not(overview: nil).where.not(vote_average: 0.0).where.not(poster_path: nil).where.not(production_countries: nil).where.not(actors: nil).where.not(overview_fr: nil)
 	end
 end
