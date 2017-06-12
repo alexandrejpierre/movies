631b8dc3b6ae677588395099e8e19dd0e3cf9214
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2017-06-12 21:59:56 +0200

    Changed the data extraction mode in the friendsfavorites api to fix the output typing issue

--- app/controllers/api/friendsfavorites_controller.rb
+++ app/controllers/api/friendsfavorites_controller.rb
@@ -6,7 +6,8 @@ module Api
 		def index
 			# 20170426: retrieval of the friends favorites from the preferences for a given user
 			if User.find_by(id: params[:user_id]).fb_connected=='Y'
-				@a=ActiveRecord::Base.connection.execute("SELECT p.tmdbmovie_id, p.user_id, u.email, u.first_name, u.name, u.picture_url, t.genres, t.overview, t.poster_path, t.production_countries, t.runtime, t.spoken_languages, t.title, t.vote_average, t.vote_count, t.release_year, t.actors, t.director, t.trailer_url, t.popularity, t.adult, t.title_fr, t.overview_fr, t.trailer_url_fr, t.friends_likes FROM preferences p INNER JOIN friendships f ON p.user_id=f.friend_id AND f.user_id=#{params[:user_id]} LEFT OUTER JOIN users u ON p.user_id=u.id LEFT OUTER JOIN tmdbmovies t ON t.id=p.tmdbmovie_id WHERE p.favorite='Y'")
+				# 20170611: Replaced base connection with find_by_sql to correct the output typing issue
+				@a=Preference.find_by_sql("SELECT p.tmdbmovie_id, p.user_id, u.email, u.first_name, u.name, u.picture_url, t.genres, t.overview, t.poster_path, t.production_countries, t.runtime, t.spoken_languages, t.title, t.vote_average, t.vote_count, t.release_year, t.actors, t.director, t.trailer_url, t.popularity, t.adult, t.title_fr, t.overview_fr, t.trailer_url_fr, t.friends_likes FROM preferences p INNER JOIN friendships f ON p.user_id=f.friend_id AND f.user_id=#{params[:user_id]} LEFT OUTER JOIN users u ON p.user_id=u.id LEFT OUTER JOIN tmdbmovies t ON t.id=p.tmdbmovie_id WHERE p.favorite='Y'")
 			else
 				@a=false
 			end
