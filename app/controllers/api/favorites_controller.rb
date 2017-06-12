4ea790955efbcf94bdc4b55000ff1e51476116ae
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2017-06-12 23:10:48 +0200

    Improved the favorites update API with a find or create

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -7,14 +7,20 @@ module Api
 		# 20170307: used the relevant_columns scope to filter the output
 		# 20170317: rollbacked relevant columns
         def index
-            #@u_id = User.find_by("email = ?",params[:email]).id
+            #obsolete: @u_id = User.find_by("email = ?",params[:email]).id
 			@favorites=Tmdbmovie.joins(:preferences).where("preferences.user_id = ?",params[:user_id]).where("preferences.favorite = 'Y'")
             respond_with @favorites, location: nil
 		end
 
 		def update
-			#@u_id=User.find_by("email = ?",update_favorites_params[:email]).id
-			@status=Preference.where("user_id = ?",update_favorites_params[:user_id]).find_by("tmdbmovie_id = ?",update_favorites_params[:id]).update_attributes(favorite: update_favorites_params[:favorite])
+			#obsolete: @u_id=User.find_by("email = ?",update_favorites_params[:email]).id
+			# 20170612: improved the API w/ a find or create in the case where the user would like to put a movie in favorites that he has not liked before
+			@preference=Preference.where(user_id: update_favorites_params[:user_id]).find_or_create_by(tmdbmovie_id: update_favorites_params[:id]) do |p|
+				p.user_id=update_favorites_params[:user_id]
+				p.tmdbmovie_id=update_favorites_params[:id]
+				p.likes='Y'
+			end
+        	@status=@preference.update_attributes(favorite: update_favorites_params[:favorite])
         	respond_with @status, location: nil
 		end
 
