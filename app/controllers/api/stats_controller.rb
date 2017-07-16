ed37a92c035af6cfaa68e098ae702e3572d6b3e8
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2017-07-16 20:59:16 +0200

    Added an output to the stats api

--- app/controllers/api/stats_controller.rb
+++ app/controllers/api/stats_controller.rb
@@ -3,8 +3,9 @@ module Api
         before_action :authenticate_token!
         respond_to :json
 		
+		# 20170716: updated the api with a group by of swipes by type
         def index
-			render json: { count_swipes: Preference.where(user_id: params[:user_id]).count, count_favorites:  Preference.where(user_id: params[:user_id]).where(favorite: 'Y').count }
+			render json: { count_swipes: Preference.where(user_id: params[:user_id]).count, count_favorites:  Preference.where(user_id: params[:user_id]).where(favorite: 'Y').count  count_swipes_by_type: Preference.where(user_id: params[:user_id]).group(:likes,:favorite).count}
 		end
 
     end
