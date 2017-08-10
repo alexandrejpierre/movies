630274c7635c3d1e30d5539f49cc5732d2a6ee1b
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2017-08-10 17:05:32 +0200

    Added a friendslikes api to see which friends liked the movie the user is proposed

---
+++ app/controllers/api/friendslikes_controller.rb
@@ -0,0 +1,12 @@
+module Api
+    class FriendslikesController < ApplicationController
+        before_action :authenticate_token!
+        respond_to :json
+		
+		def index
+			@a=User.find_by_sql("SELECT u.first_name, u.name, u.picture_url FROM users u INNER JOIN friendships f ON u.id=f.friend_id AND f.user_id=#{params[:user_id]} INNER JOIN preferences p ON p.user_id=u.id AND p.tmdbmovie_id=#{params[:tmdbmovie_id]}  WHERE p.likes='Y' ")
+			respond_with @a, location: nil
+		end
+	
+    end
+end
